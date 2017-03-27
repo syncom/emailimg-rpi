b631dd5b03cc7186f6a4a6e791e0d5ae01552486
Ning Shang <syncom.dev@gmail.com>
2017-03-27 04:09:50 -0700

    Bugfix: resolved path issues that broke the cron job.

--- email_raspistill.sh
+++ email_raspistill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+ROOTDIR=`dirname $0`
 saved_file=/mnt/ramdisk_still/capture.jpg
 raspistill -w 1280 -h 960 -t 10 -e jpg -q 15 -o ${saved_file}
-python email_image.py ${saved_file}
+python ${ROOTDIR}/email_image.py ${saved_file}
 
