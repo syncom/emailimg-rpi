b4d6e82a8bf07056d4c9838b4d2101325445309c
Ning Shang <syncom.dev@gmail.com>
2017-03-27 23:36:12 -0700

    Added timestamp info to saved file name, for better postprocessing management.

--- email_raspistill.sh
+++ email_raspistill.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ROOTDIR=`dirname $0`
-saved_file=/mnt/ramdisk_still/capture.jpg
+DATE=$(date +"%Y-%m-%d_%H%M")
+saved_file=/mnt/ramdisk_still/${DATE}.jpg
 raspistill -w 1280 -h 960 -t 10 -e jpg -q 15 -o ${saved_file}
 python ${ROOTDIR}/email_image.py ${saved_file}
-
+rm -rf ${saved_file}
