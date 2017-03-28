f4d94491f40556e00aab8e1f8d805beb0720dceb
Ning Shang <syncom.dev@gmail.com>
2017-03-28 00:01:33 -0700

    Added option for adjusting camera positioning.

--- email_raspistill.sh
+++ email_raspistill.sh
@@ -2,6 +2,7 @@
 ROOTDIR=`dirname $0`
 DATE=$(date +"%Y-%m-%d_%H%M")
 saved_file=/mnt/ramdisk_still/${DATE}.jpg
-raspistill -w 1280 -h 960 -t 10 -e jpg -q 15 -o ${saved_file}
+# set rotation 270 degrees to fit current camera positioning
+raspistill -w 1280 -h 960 -t 10 -e jpg -q 15 -rot 270 -o ${saved_file}
 python ${ROOTDIR}/email_image.py ${saved_file}
 rm -rf ${saved_file}
